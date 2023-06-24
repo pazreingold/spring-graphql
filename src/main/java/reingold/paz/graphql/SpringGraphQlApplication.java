f633e14d1dff172881241ca76a088cf387fa2d5c
pazrein <reingoldpaz@gmail.com>
2023-06-24 14:52:22 +0300

    first commit

---
+++ src/main/java/reingold/paz/graphql/SpringGraphQlApplication.java
@@ -0,0 +1,13 @@
+package reingold.paz.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGraphQlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGraphQlApplication.class, args);
+    }
+
+}
