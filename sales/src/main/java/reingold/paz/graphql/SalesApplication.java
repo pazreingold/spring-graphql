836e436980d7a19ffa30b1d547c60ecebbf350cb
pazrein <reingoldpaz@gmail.com>
2023-06-25 18:36:52 +0300

    Create Product Module

---
+++ sales/src/main/java/reingold/paz/graphql/SalesApplication.java
@@ -0,0 +1,13 @@
+package reingold.paz.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesApplication.class, args);
+    }
+
+}
